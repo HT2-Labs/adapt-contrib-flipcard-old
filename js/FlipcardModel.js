bb8d6b3e84d38ed3d61fa33d3e6442eb8c4ae0f8
cahirodoherty-learningpool <cahir.odoherty@learningpool.com>
2024-07-16 09:14:14 +0100

    Fix: Correctly setting child visited state for Flipcard items

--- js/FlipcardModel.js
+++ js/FlipcardModel.js
@@ -1,4 +1,5 @@
 import ItemsComponentModel from 'core/js/models/itemsComponentModel';
+import logging from 'core/js/logging';
 
 export default class FlipcardModel extends ItemsComponentModel {
 
@@ -9,8 +10,13 @@ export default class FlipcardModel extends ItemsComponentModel {
 
   // This function will set the visited status for particular flipcard item.
   setVisited(index) {
-    const item = this.get('_items')[index];
-    item._isVisited = true;
+    const child = this.getChildren()?.models?.[index] || null;
+    if (!child) {
+      logging.warn(`Flipcard item not found for index 'index'`);
+      return;
+    }
+
+    child.set('_isVisited', true);
     this.checkCompletionStatus();
   }
 }
