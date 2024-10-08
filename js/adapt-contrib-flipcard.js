0972b916825b63c8481199b7088672a6d3195bf2
cahirodoherty-learningpool <cahir.odoherty@learningpool.com>
2024-10-08 09:19:58 +0100

    Fake comment

--- js/adapt-contrib-flipcard.js
+++ js/adapt-contrib-flipcard.js
@@ -2,6 +2,7 @@ import components from 'core/js/components';
 import FlipcardView from './FlipcardView';
 import FlipcardModel from './FlipcardModel';
 
+// Faking a PR for test run action. Please ignore
 export default components.register('flipcard', {
   model: FlipcardModel,
   view: FlipcardView
